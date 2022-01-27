b32cc5b482d3133acf62060386684b4a09d29515
HD Vu <hadangvu2332@gmail.com>
2022-01-27 13:36:05 +0700

    day 13

---
+++ day13/src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  firstName = React.createRef()
+  lastName = React.createRef()
+  country = React.createRef()
+  title = React.createRef()
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    console.log(this.firstName.current.value)
+    console.log(this.lastName.current.value)
+    console.log(this.title.current.value)
+    console.log(this.country.current.value)
+
+    const data = {
+      firstName: this.firstName.current.value,
+      lastName: this.lastName.current.value,
+      title: this.title.current.value,
+      country: this.country.current.value,
+    }
+    console.log(data)
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <h3>Add Student</h3>
+        <form onSubmit={this.handleSubmit} className='form-styles'>
+          <div>
+            <input
+              type='text'
+              name='firstName'
+              placeholder='First Name'
+              ref={this.firstName}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <input
+              type='text'
+              name='lastName'
+              placeholder='Last Name'
+              ref={this.lastName}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <input
+              type='text'
+              name='country'
+              placeholder='Country'
+              ref={this.country}
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <input
+              type='text'
+              name='title'
+              placeholder='Title'
+              ref={this.title}
+              onChange={this.handleChange}
+            />
+          </div>
+
+          <button className='btn btn-success'>Submit</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App;
